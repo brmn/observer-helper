82a6e1798083ad4b9ce91b1148207ab7a0646250
brmn <elesin@gmail.com>
2021-09-06 20:32:04 +0300

    feat(voteabroad): hot line bot very beginning 2

---
+++ app/Models/Voteabroad/VoteabroadTicket.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Voteabroad;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VoteabroadTicket extends Model
+{
+}
