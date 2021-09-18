71c176e5638e2ed6160df66f30042afa90f79916
brmn <elesin@gmail.com>
2021-09-18 12:47:15 +0300

    feat(voteabroad): hashtag

--- app/Usecases/Voteabroad/ObserverAsksQuestion.php
+++ app/Usecases/Voteabroad/ObserverAsksQuestion.php
@@ -71,7 +71,7 @@ final class ObserverAsksQuestion
 
     private function makeMessage(Question $question): string
     {
-        return "УИК {$question->getUik()->getNumber()}({$question->getUik()->getCountry()})"
+        return "#УИК{$question->getUik()->getNumber()}(#{$question->getUik()->getCountry()})"
             . " Нужна консультация оператора: " . ($question->isNeedHelp() ? 'да' : 'нет')
             . " {$question->getObserver()->asString()}: {$question->getText()}";
     }
